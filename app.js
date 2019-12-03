da8aa9587e8eb2c8ab97cdf8730c02e2228ce3cc
Anugrah Lambogo <lambogoai@gmail.com>
2019-12-03 11:46:07 -0600

    got names to show up lol

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+/* eslint-disable indent */
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('common'));
+
+const playstore = require('./playstore-data.js');
+
+
+console.log('app is running');
+
+
+
+app.get('/apps', (req, res) => {
+    const { search } = req.query;
+    
+    let list = [];
+    for (i = 0; i < playstore.length; i++) {
+        let application = playstore[i];
+        list.push(application.App);
+    }
+
+    list.join(` `);
+
+    res.send(`${list}`);
+
+
+});
+
+
+
+app.listen(8000, () => {
+    console.log('Server is listening on port 8000!!');
+});
